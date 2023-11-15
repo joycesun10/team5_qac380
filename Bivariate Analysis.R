055c0ca278968ae768c107c930a38223bac0a5db
wlivingston89 <148150928+wlivingston89@users.noreply.github.com>
2023-11-15 15:53:33 -0500

    Add files via upload

--- Bivariate Analysis.R
+++ Bivariate Analysis.R
@@ -20,25 +20,31 @@ by(Needs$health_literacy_score, Needs$tobacco_use, mean, na.rm = TRUE)
 by(Needs$health_literacy_score, Needs$poverty, mean, na.rm = TRUE)
 
 ggplot(data=Needs)+
-  geom_boxplot(aes(x=race, y=health_literacy_score))+
-  ggtitle('Association Between Race & Health Literacy Score')
+  geom_boxplot(aes(x=race, y=health_literacy_score,color=race))+
+  ggtitle('Association Between Race & Health Literacy Score')+
+  theme(axis.text.x=element_text(angle=30,hjust=1))
+
 
 ggplot(data=Needs)+
-  geom_boxplot(aes(x=gend_id, y=health_literacy_score))+
+  geom_boxplot(aes(x=gend_id, y=health_literacy_score, color=gend_id))+
   ggtitle('Association Between Gender & Health Literacy Score')
 
 ggplot(data=Needs)+
-  geom_boxplot(aes(x=employment_status, y=health_literacy_score))+
+  geom_boxplot(aes(x=employment_status, y=health_literacy_score,color=employment_status))+
   ggtitle('Association Between Employment Status & Health Literacy Score')
 
 ggplot(data=Needs)+
-  geom_boxplot(aes(x=educ, y=health_literacy_score))+
-  ggtitle('Association Between Education & Health Literacy Score')
+  geom_boxplot(aes(x=educ, y=health_literacy_score,color=educ))+
+  ggtitle('Association Between Education & Health Literacy Score')+
+  theme(axis.text.x=element_text(angle=30,hjust=1))
+
 
 ggplot(data=Needs)+
-  geom_boxplot(aes(x=tobacco_use, y=health_literacy_score))+
+  geom_boxplot(aes(x=tobacco_use, y=health_literacy_score,color=tobacco_use))+
   ggtitle('Association Between Tobacco Use & Health Literacy Score')
 
 ggplot(data=Needs)+
-  geom_boxplot(aes(x=poverty, y=health_literacy_score))+
+  geom_boxplot(aes(x=poverty, y=health_literacy_score, color=poverty))+
   ggtitle('Association Between Poverty & Health Literacy Score')
+
+
